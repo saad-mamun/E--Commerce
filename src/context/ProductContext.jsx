43c102e7cdcf55ca8e6c5bd6d1be786ac91cd6e2
saad-mamun <mamunali88991@gmail.com>
2025-02-04 00:08:40 +0600

    data api

--- src/context/ProductContext.jsx
+++ src/context/ProductContext.jsx
@@ -1,7 +1,20 @@
-import React, { createContext, useContext } from 'react'
+import React, { createContext, useContext, useEffect } from 'react';
+import axios from "axios";
 const AppContext = createContext();
+const API = "https://jsonplaceholder.typicode.com/photos"; // video 14 data api 
+
+
 const AppProvider = ({ children }) => {
-    
+
+    const getProducts = async (url) =>{
+        const res = await axios.get(url);
+        const products = await res.data
+    }
+
+    useEffect(() => {
+        getProducts(API)
+    },[])
+
   return (
     <AppContext.Provider value={{myName: "Liton Ali"}}>
       {children}
